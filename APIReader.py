0df0d9140a245543c757f2cc595142d3ee98d16d
jpdacosta <jeanpaul.dacosta@gmail.com>
2020-11-06 17:59:13 +1100

    API code

---
+++ APIReader.py
@@ -0,0 +1,41 @@
+import requests as req
+import json 
+
+def readAPI(url_path,parameters):
+    response =""
+    if parameters == "":
+        response = req.get(url_path)
+    else:
+        response = req.get(url_path,params=parameters)
+    print(response.status_code)
+    return response
+
+def jprint(obj):
+    text = json.dumps(obj,sort_keys=True,indent=4)
+    print(text)
+
+# Space Station code---------------------------------------------------------
+# how many people in space api
+# response = readAPI("http://api.open-notify.org/astros.json","")
+# jprint(response.json())
+# How many times the space station will orbit my location
+# param = {
+#     "lat": [40.71],
+#     "lon":[-74]
+# }
+# response = readAPI(" http://api.open-notify.org/iss-pass.json",param)
+# jprint(response)
+# -----------------------------------------------------------------------------
+#WeatherMAP API----------------------------------------------------------------
+#check https://rapidapi.com/community/api/open-weather-map?endpoint=53aa6042e4b051a76d241b79 for the code
+url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
+
+querystring = {"q":"san francisco,us","lat":"35","lon":"139","cnt":"10","units":"metric"}
+
+headers = {
+    'x-rapidapi-key': "8b710e73afmshaa75416c57a4369p19b39cjsnca03a7f71899",
+    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
+    }
+
+response = req.request("GET", url, headers=headers, params=querystring)
+print(response.text)
